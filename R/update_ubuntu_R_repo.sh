8d04a65d9f252124f4d3fb93e0c1c642888a2411
Steven Pollack <steven@pollackphoto.net>
2015-04-04 10:51:46 +0200

    added GPG key acquisition and verbose messages

--- R/update_ubuntu_R_repo.sh
+++ R/update_ubuntu_R_repo.sh
@@ -11,8 +11,17 @@ echo "# Add CRAN" >> tmpDebSource
 echo $CRAN_DEB >> tmpDebSource
 sudo mv tmpDebSource $DEB_SOURCE
 
-# don't need to add GPG key for some reason...
+# add GPG as per the instructions at #secure_apt in
+# http://cran.r-project.org/bin/linux/ubuntu/
+echo "Adding Michael Rutter's public key for CRAN secure apt..."
+sudo apt-key adv --keyserver keyserver.ubuntu.com --recv-keys E084DAB9 > /dev/null
 
-echo "Updaing apt-get and (re)install r-base..."
-sudo apt-get update
-sudo apt-get install -y r-base
+echo "Updating apt-get and (re)install r-base..."
+sudo apt-get update > /dev/null
+sudo apt-get install -y r-base-core > /dev/null
+
+# print version number and location
+R_VERSION=$(Rscript -e "cat(R.version.string, fill = TRUE)")
+echo $R_VERSION installed to $(which R)
+
+echo "update_ubuntu_R_repo.sh: done..."
