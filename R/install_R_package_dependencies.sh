6384a9f9295c643ec88d602c23f60f5392c8232d
Steven Pollack <steven@gnobel.com>
2015-05-19 18:10:02 +0200

    changed INSTALL_SCRIPT and created symlink
    * install dev versions of VimCom and colorout
    * reorder installation to not double-back on itself

--- R/install_R_package_dependencies.sh
+++ R/install_R_package_dependencies.sh
@@ -7,21 +7,43 @@ echo "Installing headers for 'RMySQL' and 'RPostgres'..."
 sudo apt-get install -y libpq-dev > /dev/null
 sudo apt-get install -y libmysqlclient-dev > /dev/null
 
-echo "Installing 'devtools', 'data.table', 'ggplot2', 'reshape2', 'string', 'Rcpp', 'DBI', 'RMySQL', 'RPostgres'..."
+echo "Installing headers for 'vimcom'..."
+sudo apt-get install -y libx11-dev > /dev/null
+
+echo "Installing 'devtools', 'data.table', 'ggplot2', 'reshape2', 'string', 'Rcpp', 'DBI', 'RMySQL', 'RPostgres', 'vimcom', 'colorout', 'setwidth'..."
 INSTALL_SCRIPT='options(repos = c(CRAN = "http://cran.rstudio.com")); 
 install.packages("devtools"); 
+install.packages("stringr"); 
+devtools::install_github("RcppCore/Rcpp"); 
 install.packages("data.table"); 
 install.packages("ggplot2"); 
 install.packages("reshape2"); 
-install.packages("stringr"); 
-devtools::install_github("RcppCore/Rcpp"); 
 devtools::install_github("rstats-db/DBI"); 
 devtools::install_github("rstats-db/RMySQL"); 
-devtools::install_github("rstats-db/RPostgres");'
+devtools::install_github("rstats-db/RPostgres");
+devtools::install_github("jalvesaq/VimCom");
+devtools::install_github("jalvesaq/colorout");
+install.packages("setwidth");'
 
 echo $INSTALL_SCRIPT >> tmp.R
 
 sudo Rscript -e "source('tmp.R')"
 rm tmp.R
 
-echo "install_R_package_dependencies.sh finished. This does not mean that all packages were successfully installed, though..."
+echo "creating .Rprofile symlink..."
+# this script is either being called from $cloaked-wight or
+# $cloaked-wight/R
+RPROFILE=$(pwd)/R/.Rprofile
+if [ ! -f $RPROFILE ]; then # we're being called from $cloaked-wight/vim
+  RPROFILE=$(pwd)/.Rprofile
+fi
+
+if [ ! -f $RPROFILE ]; then # this is being called from someplace else!
+  echo "must call script from repo top-level or R/ sub-dir"
+  exit
+fi
+
+sudo ln -fs $RPROFILE ~/.Rprofile
+echo Linked ~/.Rprofile "->" $RPROFILE ...
+
+echo "install_R_package_dependencies.sh: done..."
