687eb5b42fb32b5eac329a0c8f696a60d459a643
Steven Pollack <steven@pollackphoto.net>
2015-05-30 19:40:39 +0200

    refactored to yield proper exit codes

--- R/install_R_package_dependencies.sh
+++ R/install_R_package_dependencies.sh
@@ -1,14 +1,27 @@
+
+FAILED_LIBS=""
+EXIT_CODE=0
+
 echo "Installing headers for 'XML', 'git2r' and 'RCurl'..."
-sudo apt-get install -y libxml2-dev > /dev/null || \
-  { echo "libxm2-dev failed to install..."; exit 1 }
+sudo apt-get install -y libxml2-dev > /dev/null || {
+  FAILED_LIBS=$(echo $FAILED_LIBS "libxm2-dev");
+  EXIT_CODE=1;
+}
 
-sudo apt-get install -y libgit2-dev > /dev/null || \
-  { echo "libgit2-dev failed to install..."; exit 1 }
+#sudo apt-get install -y libgit2-dev > /dev/null || {
+#  FAILED_LIBS=$(echo $FAILED_LIBS "libgit2-dev");
+#  EXIT_CODE=1;
+#}
 
+sudo apt-get install -y libcurl4-openssl-dev > /dev/null || {
+  FAILED_LIBS=$(echo $FAILED_LIBS "libcurl4-openssl-dev");
+  EXIT_CODE=1;
+}
 
-sudo apt-get install -y libcurl4-openssl-dev > /dev/null || \
-  { echo "libcurl4-openssl-dev failed to install..."; exit 1 }
-
+if [ $EXIT_CODE -eq 1 ]; then
+  echo $FAILED_LIBS "failed to install..."
+  exit $EXIT_CODE
+fi
 
 echo "Installing headers for 'RMySQL' and 'RPostgres'..."
 sudo apt-get install -y libpq-dev > /dev/null
@@ -34,8 +47,16 @@ install.packages("setwidth");'
 
 echo $INSTALL_SCRIPT >> tmp.R
 
-sudo Rscript -e "source('tmp.R')"
+sudo Rscript -e "source('tmp.R')" || {
+  echo "One or more R packages failed to install...";
+  EXIT_CODE=1;
+}
+
 rm tmp.R
+
+if [ $EXIT_CODE -eq 1 ]; then
+  exit $EXIT_CODE
+fi
 
 echo "creating .Rprofile symlink..."
 # this script is either being called from $cloaked-wight or
@@ -54,3 +75,4 @@ sudo ln -fs $RPROFILE ~/.Rprofile
 echo Linked ~/.Rprofile "->" $RPROFILE ...
 
 echo "install_R_package_dependencies.sh: done..."
+exit $EXIT_CODE
--- R/update_ubuntu_R_repo.sh
+++ R/update_ubuntu_R_repo.sh
@@ -21,13 +21,26 @@ sudo mv tmpDebSource $DEB_SOURCE
 echo "Adding Michael Rutter's public key for CRAN secure apt..."
 sudo apt-key adv --keyserver keyserver.ubuntu.com --recv-keys E084DAB9 > /dev/null
 
+EXIT_CODE=0
+
 echo "Updating apt-get and (re)install r-base..."
 sudo apt-get update > /dev/null
-sudo apt-get install -y r-base > /dev/null || { echo "r-base failed to install..."; exit 1 }
+sudo apt-get install -y r-base > /dev/null || {
+  echo "r-base failed to install...";
+  EXIT_CODE=1;
+}
 
+if [ $EXIT_CODE -eq 1 ]; then
+  exit $EXIT_CODE
+fi
 
 # print version number and location
 R_VERSION=$(Rscript -e "cat(R.version.string, fill = TRUE)")
-echo $R_VERSION installed to $(which R)
+EXIT_CODE=$?
+
+if [ $EXIT_CODE -eq 0 ]; then
+  echo $R_VERSION installed to $(which R)
+fi
 
 echo "update_ubuntu_R_repo.sh: done..."
+exit $EXIT_CODE
