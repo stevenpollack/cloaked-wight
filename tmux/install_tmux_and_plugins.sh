5d5e9c398f15402cafe0748d9403a35fe40219b6
Steven Pollack <steven@pollackphoto.net>
2015-05-30 16:51:45 +0200

    refactored tmux install
    * use Rscript to determine if installed tmux version is sufficient

--- tmux/install_tmux_and_plugins.sh
+++ tmux/install_tmux_and_plugins.sh
@@ -7,32 +7,60 @@
 
 echo "install_tmux_and_plugins.sh: starting..."
 
-echo "Checking for tmux..."
-tmux -V 2>&1 > /dev/null # suppress output
-TMUX_IS_AVAILABLE=$?
-if [ ! $TMUX_IS_AVAILABLE -eq 0 ]; then
-  echo "tmux is not installed... Will get with sudo apt-get..."
-  sudo apt-get install -y tmux > /dev/null
-else
-  echo $(tmux -V) "is installed..." 
-fi
-
-# tmux (1.8) ships with ubuntu 14.04, so we just check the version 
-# check for tmux v1.9a
-echo "Checking tmux version..."
-if [ $(tmux -V | grep -c "1.9") -eq 1 ]; then
-  echo "tmux (>= 1.9) is installed."
-else
-  echo "Will install v2.0-1 now..."
-  # sudo apt-get update
+# helper functions:
+function install_tmux {
+  # add pi-rho/dev PPA for latest version of tmux
   sudo apt-get install -y python-software-properties software-properties-common > /dev/null
   sudo add-apt-repository -y ppa:pi-rho/dev > /dev/null
   sudo apt-get update > /dev/null
-  sudo apt-get install -y tmux > /dev/null
-  if [ $(tmux -V | grep -c "2\\.0") -eq 0 ]; then
-    echo "tmux 2.0-1 failed to install..."
-    exit 1
+
+  if [ $1 -eq "trusty" ]; then
+    sudo apt-get install -y tmux=2.0-1~ppa1~t > /dev/null
+    if [ $(tmux -V | grep -c "2\\.0") -eq 0 ]; then
+      echo "tmux 2.0-1 failed to install..."
+      exit 1
+    fi
+  else
+    # we're in checking build from travis which is at 12.04
+    sudo apt-get install -y tmux=1.9a-1~ppa1~p  > /dev/null
+    if [ $(tmux -V | grep -c "1\\.9") -eq 0 ]; then
+      echo "tmux 1.9a-1 failed to install..."
+      exit 1
+    fi 
   fi
+}
+
+function check_tmux_version {
+  tmux -V 2>&1 > /dev/null # suppress output
+  TMUX_IS_AVAILABLE=$?
+
+  if [ $TMUX_IS_AVAILABLE -eq 0 ]; then
+    export TMUX_VERSION=$(tmux -V | sed -e 's/tmux\s*//g')
+    echo $(Rscript -e 'cat(Sys.getenv("TMUX_VERSION") > "1.9")')
+  else
+    echo "FALSE"
+  fi
+}
+
+echo "Checking for tmux..."
+tmux -V 2>&1 > /dev/null # suppress output
+TMUX_IS_AVAILABLE=$?
+
+# get ubuntu version in the case we need to install or upgrade tmux
+UBUNTU_VERSION=$()
+
+VERSION_IS_GOOD=check_tmux_version
+
+if [[ ! "$VERSION_IS_GOOD" = "TRUE" ]]; then
+  echo "Installig the latest version of tmux in pi-rho PPA..."
+  install_tmux $UBUNTU_VERSION
+  VERSION_IS_GOOD=check_tmux_version
+fi
+
+# double check that everything got installed properly
+if [[ ! $VERSION_IS_GOOD = "TRUE" ]]; then
+  echo "Latest version of tmux failed to properly install..."
+  exit 1
 fi
 
 echo "Checking for xclip..."
@@ -40,7 +68,8 @@ xclip -version 2>&1 >/dev/null # suppress output
 XCLIP_IS_AVAILABLE=$?
 if [ ! $XCLIP_IS_AVAILABLE -eq 0 ]; then
   echo "xclip is not installed... Will get with sudo apt-get..."
-  sudo apt-get install -y xclip > /dev/null
+  sudo apt-get install -y xclip > /dev/null || \
+  { echo "xclip failed to install..."; exit 1 } 
 else
   echo $(xclip -version) is installed to $(which xclip)
 fi
