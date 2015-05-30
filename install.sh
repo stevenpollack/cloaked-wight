f9e645b1621000c35a50365cc8245f0473133da5
Steven Pollack <steven@pollackphoto.net>
2015-05-30 21:02:04 +0200

    made failure verbose

--- install.sh
+++ install.sh
@@ -4,15 +4,48 @@
 # 1. install JDK 7 and wget postgres JDBC driver
 # 2. zsh
 # 3. gvim
-# 4. tmux
 # 5. update repo for R and install latest version
 # 6. install desired R packages and necessary library headers
+# 4. tmux 
 
 sudo sh remove_translation_packages.sh 
-sudo sh misc/install_jdk_and_postgres_driver.sh
-sudo sh zsh/install_git_and_zsh.sh
-sudo sh vim/install_gvim_and_plugins.sh
-sudo sh R/update_ubuntu_R_repo.sh
-sudo sh R/install_R_package_dependencies.sh
-sudo sh tmux/install_tmux_and_plugins.sh
+if [ ! $? -eq 0 ]; then
+  echo "remove_translation_packages.sh failed..."
+  exit 1
+fi
 
+sudo sh misc/install_jdk_and_postgres_driver.sh
+if [ ! $? -eq 0 ]; then
+  echo "misc/install_jdk_and_postgres_driver.sh failed..."
+  exit 1
+fi
+
+sudo sh zsh/install_git_and_zsh.sh
+if [ ! $? -eq 0 ]; then
+  echo "zsh/install_git_and_zsh.sh failed..."
+  exit 1
+fi
+
+sudo sh vim/install_gvim_and_plugins.sh
+if [ ! $? -eq 0 ]; then
+  echo "vim/install_gvim_and_plugins.sh failed..."
+  exit 1
+fi
+
+sudo sh R/update_ubuntu_R_repo.sh
+if [ ! $? -eq 0 ]; then
+  echo "R/update_ubuntu_R_repo.sh failed..."
+  exit 1
+fi
+
+sudo sh R/install_R_package_dependencies.sh
+if [ ! $? -eq 0 ]; then
+  echo "R/install_R_package_dependencies.sh failed..."
+  exit 1
+fi
+
+sudo sh tmux/install_tmux_and_plugins.sh
+if [ ! $? -eq 0 ]; then
+  echo "tmux/install_tmux_and_plugins.sh failed..."
+  exit 1
+fi
