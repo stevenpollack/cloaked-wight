60277b154ab098d2d3ee587b20a81170701bf2f7
Steven Pollack <steven@gnobel.com>
2015-08-16 23:57:59 +0200

    revert installation order

--- install.sh
+++ install.sh
@@ -4,9 +4,9 @@
 # 1. install JDK 7 and wget postgres JDBC driver
 # 2. zsh
 # 3. gvim
-# 4. tmux 
 # 5. update repo for R and install latest version
 # 6. install desired R packages and necessary library headers
+# 4. tmux -- depends on R, for version checks
 
 sudo sh remove_translation_packages.sh 
 if [ ! $? -eq 0 ]; then
@@ -32,12 +32,6 @@ if [ ! $? -eq 0 ]; then
   exit 1
 fi
 
-sudo sh tmux/install_tmux_and_plugins.sh
-if [ ! $? -eq 0 ]; then
-  echo "tmux/install_tmux_and_plugins.sh failed..."
-  exit 1
-fi
-
 sudo sh R/update_ubuntu_R_repo.sh
 if [ ! $? -eq 0 ]; then
   echo "R/update_ubuntu_R_repo.sh failed..."
@@ -47,5 +41,11 @@ fi
 sudo sh R/install_R_package_dependencies.sh
 if [ ! $? -eq 0 ]; then
   echo "R/install_R_package_dependencies.sh failed..."
+  exit 1
+fi 
+
+sudo sh tmux/install_tmux_and_plugins.sh
+if [ ! $? -eq 0 ]; then
+  echo "tmux/install_tmux_and_plugins.sh failed..."
   exit 1
 fi 
