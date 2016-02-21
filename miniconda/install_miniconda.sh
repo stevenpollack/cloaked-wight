728131a9f306c29ea27be612d9f87eb57ae403d7
Steven Pollack <steven@gnobel.com>
2016-02-21 18:55:36 +0100

    expose updated $PATH to other scripts

--- miniconda/install_miniconda.sh
+++ miniconda/install_miniconda.sh
@@ -5,15 +5,19 @@ miniconda=Miniconda3-latest-Linux-x86_64.sh
 anaconda_dir=/opt/anaconda
 anaconda_bin=$anaconda_dir/bin
 
-if [[ ! -f $miniconda ]]; then
-  wget --quiet http://repo.continuum.io/miniconda/$miniconda
+if [ ! -e $miniconda ]; then
+  wget http://repo.continuum.io/miniconda/$miniconda
 fi
 
 chmod +x $miniconda
-$miniconda -b -p $anaconda_dir 
+bash $miniconda -b -p $anaconda_dir 
 
+# add anaconda bin to zshrc and export it for future scripts
 cat >> ~/.zshrc << END
 # add for anaconda install
 export PATH=$anaconda_bin:\$PATH
 
 END
+
+export PATH=$anaconda_bin:$PATH
+
