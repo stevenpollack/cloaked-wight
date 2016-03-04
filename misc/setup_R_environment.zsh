6ff589d2c3bae2189c968b80a1cadcd3c8d42b47
Steven Pollack <steven@gnobel.com>
2016-03-04 23:46:48 +0100

    separated script to create R environment with conda

---
+++ misc/setup_R_environment.zsh
@@ -0,0 +1,31 @@
+#!/usr/bin/env zsh
+
+# setup R environment
+echo "Creating conda-R environment..."
+if [ "$SHELL" != '/usr/bin/zsh' ]; then
+  source ~/.zshrc
+fi
+
+conda install -y --channel r \
+  jupyter \
+  r-irkernel \
+  r-data.table \
+  r-devtools \
+  r-ggplot2 \
+  r-stringr
+
+echo "Installing packages with devtools..." 
+cat > tmp.R <<EOT
+options(unzip = 'internal',
+        repos = c(CRAN = "https://cran.rstudio.com")); 
+devtools::install_github(c("renkun-ken/pipeR")); 
+EOT
+
+Rscript tmp.R 
+EXIT_CODE=$?  
+rm tmp.R 
+
+if [ ! EXIT_CODE -eq 0 ]; then
+  exit 1
+fi
+
